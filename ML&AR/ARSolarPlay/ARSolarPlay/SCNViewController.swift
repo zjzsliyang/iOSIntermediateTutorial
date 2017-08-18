6a33bb060890101b3a1faec82f23ee268c35768e
Yang LI <zjzsliyang@gmail.com>
2017-08-18 22:02:54 +0800

    add project
    add project & delete dead code

---
+++ ML&AR/ARSolarPlay/ARSolarPlay/SCNViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SCNViewController.swift
+//  ARSolarPlay
+//
+//  Created by Yang Li on 18/08/2017.
+//  Copyright © 2017 Yang Li. All rights reserved.
+//
+
+import UIKit
+
+class SCNViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
